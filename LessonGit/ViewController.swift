191697898778f00688a24b6f0aa3f9db21783df1
17586317 <Borisov.S.Alek@sberbank.ru>
2020-07-25 14:53:21 +0300

    Initial Commit

---
+++ LessonGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LessonGit
+//
+//  Created by 17586317 on 25.07.2020.
+//  Copyright © 2020 Борисов Сергей Александрович. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
